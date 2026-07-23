11fbd8cf23f5a19fb2e7dba856e204f795944259
Rodion <rodionvitenberg@gmail.com>
2026-07-23 21:47:15 +0600

    2

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
